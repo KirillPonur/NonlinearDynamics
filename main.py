669c99fd967a690564e854381a7615c850defce4
Unknown <ponur0kirill@gmail.com>
2019-07-21 00:33:07 +0300

    lect4 + извлечение рисунков

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import os
 svg=[]
-for file in os.listdir(path='fig'):
+for file in os.listdir(path='fig\\lect5'):
 	if file.endswith('.svg'):
 		svg.append(file[0:-4])
 		# print(svg)
 for file in svg:
-	os.system('D:/Inkscape/inkscape --file=fig/'+file+'.svg --export-pdf=fig/'+file+'.pdf --export-latex')
+	os.system('D:/Inkscape/inkscape --file=fig\\lect5\\'+file+'.svg --export-pdf=fig\\lect5\\'+file+'.pdf --export-latex')
